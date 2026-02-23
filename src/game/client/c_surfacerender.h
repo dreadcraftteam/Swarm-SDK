257249d25607a891e1d0da4b3176108b614b727f
dreadcraftteam <dreadcraftteam@gmail.com>
2026-02-23 12:53:43 +0500

    Cleanup?

--- src/game/client/c_surfacerender.h
+++ src/game/client/c_surfacerender.h
@@ -1,8 +1,6 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
 //
 // Purpose: Blobulator surface rendering
-// 
-// By VALVe and heavily modified by DREADCRAFT
 //
 //========================================================================//
 
@@ -15,20 +13,5 @@
 #include "view_shared.h"
 #include "iviewrender.h"
 #include "engine/ivdebugoverlay.h"
-
-#ifdef SDK_USE_BLOBULATOR
-#include "../../common/blobulator/Implicit/ImpDefines.h"
-#include "../../common/blobulator/Implicit/ImpRenderer.h"
-#include "../../common/blobulator/Implicit/ImpTiler.h"
-#include "../../common/blobulator/Implicit/UserFunctions.h"
-
-void Surface_DrawSpheres(IMaterial* pMaterial, float flRadius);
-void Surface_Draw( IClientRenderable *pClientRenderable, const Vector &vecRenderOrigin, IMaterial *pMaterial, float flCubeWidth, bool bSurfaceNoParticleCull = false );
-void Surface_SafeLightCubeUpdate( const Vector &vecRenderOrigin, Vector4D *cachedCubeColours );
-
-extern CUtlVector<ImpParticleWithFourInterpolants, CUtlMemoryAligned<ImpParticleWithFourInterpolants, 16>> g_SurfaceRenderParticles;
-extern const QAngle g_SurfaceRenderAnglesAngles;
-
-#endif // SDK_USE_BLOBULATOR
 
 #endif // C_SURFACERENDER_H
